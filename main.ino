5d9e0768a90f92614df824c0c34f7ca382b88659
Nguyễn Anh Tuấn <tuan.nguyen2208nat@hcmut.edu.vn>
2024-07-31 23:23:21 +0700

    update code

--- main.ino
+++ main.ino
@@ -1,88 +1,103 @@
 #include <WiFi.h>
-#include <WebServer.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <LittleFS.h>
+#include <Wire.h>
 #include <DHT20.h>
 #include "config.h"
 
+DHT20 dht20;
+int led = D7;
+AsyncWebServer server(httpPort);
+AsyncWebSocket ws("/ws");
+
+// Task to read temperature and humidity
 void TaskTemperatureHumidity(void *pvParameters);
 
-WebServer server(httpPort);
-DHT20 dht20;
-
-// Variables to hold the temperature and humidity readings
-float temperature = 0.0;
-float humidity = 0.0;
-
-void handleRoot()
-{
-  String html = "<html><body><h1>ESP32 Web Server</h1>";
-  html += "<p><a href=\"/on\"><button>Turn ON LED</button></a></p>";
-  html += "<p><a href=\"/off\"><button>Turn OFF LED</button></a></p>";
-  html += "<p>Temperature: " + String(temperature) + " °C</p>";
-  html += "<p>Humidity: " + String(humidity) + " %</p>";
-  html += "</body></html>";
-  server.send(200, "text/html", html);
-}
-
-int led = 10;
-
-void handleLEDOn()
-{
-  digitalWrite(led, HIGH);
-  digitalWrite(LED_BUILTIN, HIGH);
-  server.send(200, "text/html", "LED is ON");
-}
-
-void handleLEDOff()
-{
-  digitalWrite(led, LOW);
-  digitalWrite(LED_BUILTIN, LOW);
-  server.send(200, "text/html", "LED is OFF");
-}
-
-void setup()
-{
+void setup() {
   Serial.begin(115200);
-  pinMode(LED_BUILTIN, OUTPUT); // Cài đặt chân LED là output
-  pinMode(led, OUTPUT);
-  dht20.begin();
-  
-  // Kết nối WiFi với IP tĩnh
-  if (!WiFi.config(local_IP, gateway, subnet))
-  {
-    Serial.println("STA Failed to configure");
+  pinMode(led,OUTPUT);
+
+  if (!LittleFS.begin()) {
+    Serial.println("An Error has occurred while mounting LittleFS");
+    return;
   }
 
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED)
-  {
+  while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
-    Serial.print(".");
+    Serial.println("Connecting to WiFi...");
   }
-  Serial.println("\nConnected to WiFi");
+
+  Serial.println("Connected to WiFi");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
-  server.on("/", handleRoot);
-  server.on("/on", handleLEDOn);
-  server.on("/off", handleLEDOff);
+  // Start DHT sensor
+  dht20.begin();
 
+  // Set up WebSocket events
+  ws.onEvent(onEvent);
+  server.addHandler(&ws);
+
+  // Serve the HTML page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(LittleFS, "/index.html", "text/html");
+  });
+
+  // Serve the JavaScript file
+  server.on("/script.js", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(LittleFS, "/script.js", "application/javascript");
+  });
+
+  // Serve the CSS file
+  server.on("/styles.css", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(LittleFS, "/styles.css", "text/css");
+  });
+
+  // Start server
   server.begin();
-  Serial.println("HTTP server started");
-  xTaskCreate(TaskTemperatureHumidity, "Task Temperature", 2048, NULL, 2, NULL);
+
+  // Start the temperature and humidity task
+  xTaskCreate(TaskTemperatureHumidity, "TaskTemperatureHumidity", 2048, NULL, 2, NULL);
+  Serial.println("Task started");
 }
 
-void loop()
-{
-  server.handleClient();
+void loop() {
+  // The main loop can be used for other tasks if necessary
 }
 
-void TaskTemperatureHumidity(void *pvParameters)
-{ // This is a task.
-  while (1)
-  {
+void TaskTemperatureHumidity(void *pvParameters) {
+  while (1) {
+    // Read data from the DHT20 sensor
     dht20.read();
-    temperature = dht20.getTemperature();
-    humidity = dht20.getHumidity();
+    float temperature = dht20.getTemperature();
+    float humidity = dht20.getHumidity();
+    if (ws.count() > 0) {
+      String data = String("{\"temperature\":") + temperature + ",\"humidity\":" + humidity + "}";
+      ws.textAll(data);
+    }
+
+    // Wait for 5 seconds before reading again
     delay(5000);
+  }
+}
+
+void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len) {
+  if (type == WS_EVT_CONNECT) {
+    Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+  } else if (type == WS_EVT_DISCONNECT) {
+    Serial.printf("WebSocket client #%u disconnected\n", client->id());
+  } else if (type == WS_EVT_DATA) {
+    AwsFrameInfo *info = (AwsFrameInfo*)arg;
+    if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
+      data[len] = 0;
+      Serial.printf("%s\n", (char*)data);
+      if (strcmp((char*)data, "toggleON") == 0) {
+        Serial.println("Turn LED ON");
+      } else if (strcmp((char*)data, "toggleOFF") == 0) {
+        Serial.println("Turn LED OFF");
+      }
+    }
   }
 }
