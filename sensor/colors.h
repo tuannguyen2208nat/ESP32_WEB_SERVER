08f3113b430a79ee1b5fdc0b12eb06e14033492d
Nguyễn Anh Tuấn <tuan.nguyen2208nat@hcmut.edu.vn>
2024-08-06 00:08:47 +0700

    add sensors

---
+++ sensor/colors.h
@@ -0,0 +1,16 @@
+#ifndef COLORS_H
+#define COLORS_H
+
+#include <Adafruit_NeoPixel.h>
+
+const uint32_t OFF = Adafruit_NeoPixel::Color(0, 0, 0);
+const uint32_t RED = Adafruit_NeoPixel::Color(255, 0, 0);
+const uint32_t ORANGE = Adafruit_NeoPixel::Color(255, 102, 0);
+const uint32_t YELLOW = Adafruit_NeoPixel::Color(255, 255, 0);
+const uint32_t GREEN = Adafruit_NeoPixel::Color(0, 255, 0);
+const uint32_t BLUE = Adafruit_NeoPixel::Color(0, 0, 255);
+const uint32_t INDIGO = Adafruit_NeoPixel::Color(75, 0, 130);
+const uint32_t PURPLE = Adafruit_NeoPixel::Color(128, 0, 128);
+const uint32_t WHITE = Adafruit_NeoPixel::Color(255, 255, 255);
+const uint32_t BLACK = Adafruit_NeoPixel::Color(0, 0, 0);
+#endif
--- sensor/sensor.ino
+++ sensor/sensor.ino
@@ -1,23 +1,40 @@
 #include <Wire.h>
 #include <DHT20.h>
+#include "colors.h"
+#include <Ultrasonic.h>
+#include <ESP32Servo.h>
+#include <Adafruit_NeoPixel.h>
+#include <LiquidCrystal_I2C.h>
 
-DHT20 dht20;
 
-void TaskTemperatureHumidity(void *pvParameters);
+void TaskLCD(void *pvParameters);
+void TaskMFM(void *pvParameters); //Mini fan motor
+void TaskSMS(void *pvParameters); //Soil moisture 
+void TaskPIR(void *pvParameters); //Passive infrared sensor
+void TaskUSM(void *pvParameters); //Ultrasonic sensor module
+void Task2CSM(void *pvParameters); //2-Channel switching module
+void TaskServo(void *pvParameters);
 void TaskRelay(void *pvParameters);
 void TaskLight(void *pvParameters);
-void TaskPIR(void *pvParameters); //Passive infrared sensor
-void TaskMFM(void *pvParameters); //Mini fan motor
+void TaskLedRGB(void *pvParameters);
+void TaskTemperatureHumidity(void *pvParameters);
 
 
 void setup() {
   Serial.begin(115200);
+  Wire.begin();
 
-  xTaskCreate(TaskTemperatureHumidity, "TaskTemperatureHumidity", 2048, NULL, 2, NULL);
+  xTaskCreate(TaskLCD, "TaskLCD", 2048, NULL, 2, NULL);
+  xTaskCreate(TaskMFM, "TaskMFM", 2048, NULL, 2, NULL);
+  xTaskCreate(TaskSMS, "TaskSMS", 2048, NULL, 2, NULL);
+  xTaskCreate(TaskPIR, "TaskPir", 2048, NULL, 2, NULL);
+  xTaskCreate(TaskUSM, "TaskUSM", 2048, NULL, 2, NULL);
+  xTaskCreate(Task2CSM, "Task2CSM", 2048, NULL, 2, NULL);
+  xTaskCreate(TaskServo, "TaskServo", 2048, NULL, 2, NULL);
   xTaskCreate(TaskRelay, "TaskRelay", 2048, NULL, 2, NULL);
   xTaskCreate(TaskLight, "TaskLight", 2048, NULL, 2, NULL);
-  xTaskCreate(TaskPIR, "TaskPir", 2048, NULL, 2, NULL);
-  xTaskCreate(TaskMFM, "TaskMFM", 2048, NULL, 2, NULL);
+  xTaskCreate(TaskLedRGB, "TaskLedRGB", 2048, NULL, 2, NULL);
+  xTaskCreate(TaskTemperatureHumidity, "TaskTemperatureHumidity", 2048, NULL, 2, NULL);
 
   Serial.println("Start");
 }
@@ -25,22 +42,107 @@ void setup() {
 void loop() {
   Serial.println();
   delay(5000);
-
 }
 
-void TaskTemperatureHumidity(void *pvParameters) {
-  dht20.begin();
+void TaskLCD(void *pvParameters) {
+  LiquidCrystal_I2C lcd(33,16,2);
+  lcd.begin();
 
   while (1) {
-    dht20.read();
-    Serial.println("Temperature : " + String(dht20.getTemperature())+ " - " + "Humidity : " + String(dht20.getHumidity()));
-    vTaskDelay(5000 / portTICK_PERIOD_MS); 
+  lcd.setCursor((16 - 5) / 2, 0);
+  lcd.print("Hello");  
+  lcd.setCursor(0,1);
+  lcd.print("My name is Tuan");  
+  vTaskDelay(5000 / portTICK_PERIOD_MS); 
+  lcd.clear();
+  vTaskDelay(5000 / portTICK_PERIOD_MS); 
+  }
+}
+
+void TaskMFM(void *pvParameters) {
+  int sensorPin = D9;
+  pinMode(sensorPin, OUTPUT);
+  bool check = true;
+
+  while (1) {
+  digitalWrite(sensorPin, check ? HIGH : LOW); 
+  Serial.println("MFM : " + String(check ? "ON" : "OFF"));
+  check = !check;  
+  vTaskDelay(5000 / portTICK_PERIOD_MS); 
+  }
+}
+
+void TaskSMS(void *pvParameters) {
+  int sensorPin = D3;
+
+  while (1) {
+  Serial.println("Soil moisture : " + String(analogRead(sensorPin)) +" %");
+  vTaskDelay(5000 / portTICK_PERIOD_MS); 
+  }
+}
+
+void TaskPIR(void *pvParameters) {
+  int sensorPin = D7;
+  pinMode(sensorPin, INPUT);
+
+  while (1) {
+  Serial.println("PIR : " + String(analogRead(sensorPin)?"detect":"not detect"));
+  vTaskDelay(5000 / portTICK_PERIOD_MS); 
+  }
+}
+
+void TaskUSM(void *pvParameters) {
+  int sensorPin_Left = D9; 
+  int sensorPin_Right = D10; 
+  Ultrasonic ultrasonic(sensorPin_Left, sensorPin_Right);
+
+  while (1) {
+  Serial.println("Distance : " + String(ultrasonic.read()) + " cm");
+  vTaskDelay(5000 / portTICK_PERIOD_MS); 
+  }
+}
+
+void Task2CSM(void *pvParameters) {
+  int sensorPin = D5; 
+  pinMode(sensorPin, OUTPUT);
+  bool check = true;
+
+  while (1) {
+  digitalWrite(sensorPin, check ? HIGH : LOW);
+  Serial.println("2CSM : " + String(check ? "ON" : "OFF"));
+  check = !check;
+  vTaskDelay(5000 / portTICK_PERIOD_MS); 
+  }
+}
+
+void TaskServo(void *pvParameters) {
+  int sensorPin = D2; 
+  int pos = 0;
+  Servo myservo;
+  myservo.attach(sensorPin);
+  myservo.write(90);
+  bool check = true;
+
+  while (1) {
+  if(check)
+  {
+    for (pos=0; pos<=180; pos++) { 
+    myservo.write(pos);
+    }
+  }
+  else{
+  for (pos=180; pos>=0; pos--) {
+    myservo.write(pos);
+  }
+  }
+  check=!check;
+  vTaskDelay(5000 / portTICK_PERIOD_MS); 
   }
 }
 
 void TaskRelay(void *pvParameters) {
   int sensorPin = D3;
-  pinMode(D3, OUTPUT); 
+  pinMode(sensorPin, OUTPUT); 
   bool check = true;
 
   while (1) {
@@ -60,26 +162,39 @@ void TaskLight(void *pvParameters) {
   }
 }
 
-void TaskPIR(void *pvParameters) {
-  int sensorPin = D7;
-  pinMode(sensorPin, INPUT);
+void TaskLedRGB(void *pvParameters) {
+  int sensorPin = D7; 
+  Adafruit_NeoPixel rgb(4, sensorPin, NEO_GRB + NEO_KHZ800);
+  uint32_t colors[] = {RED, ORANGE, YELLOW, GREEN, BLUE, INDIGO, PURPLE, WHITE, BLACK, OFF};
+  const char* colorNames[] = {"RED", "ORANGE", "YELLOW", "GREEN", "BLUE", "INDIGO", "PURPLE", "WHITE", "BLACK", "OFF"};
+  int numColors = sizeof(colors) / sizeof(colors[0]);
+  rgb.begin();
 
   while (1) {
-  Serial.println("PIR : " + String(analogRead(sensorPin)?"detect":"not detect"));
+  for (int i = 0; i < numColors; i++) {
+  for (int j = 0; j < 4; j++) {
+  rgb.setPixelColor(j, colors[i]);
+  }
+  rgb.show();
+  Serial.println("Led RGB : " + String(colorNames[i]));
   vTaskDelay(5000 / portTICK_PERIOD_MS); 
+  }
   }
 }
 
-void TaskMFM(void *pvParameters) {
-  int sensorPin = D9;
-  pinMode(sensorPin, OUTPUT);
-  bool check = true;
+void TaskTemperatureHumidity(void *pvParameters) {
+  DHT20 dht20;
+  dht20.begin();
 
   while (1) {
-  digitalWrite(sensorPin, check ? HIGH : LOW); 
-  Serial.println("MFM : " + String(check ? "ON" : "OFF"));
-  check = !check;  
-  vTaskDelay(5000 / portTICK_PERIOD_MS); 
+    dht20.read();
+    Serial.println("Temperature : " + String(dht20.getTemperature())+ " - " + "Humidity : " + String(dht20.getHumidity()));
+    vTaskDelay(5000 / portTICK_PERIOD_MS); 
   }
 }
+
+
+
+
+
 
